e940134ecd6e77c528b96eddf0377b8c58f76052
KLuka <klukasocial@gmail.com>
2016-03-17 09:27:47 +0100

    enable the creation of cocktail_recipes with it's ingredients

--- app/models/cocktail_recipe_part.rb
+++ app/models/cocktail_recipe_part.rb
@@ -1,5 +1,5 @@
 class CocktailRecipePart < ActiveRecord::Base
-  belongs_to :cocktail_recipe
+  belongs_to :cocktail_recipe, inverse_of: :cocktail_recipe_parts
   belongs_to :ingredient_category
   belongs_to :ingredient # The Default ingredient
 
