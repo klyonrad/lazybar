294b9ccf05c43942e5a0bdf3939b14deeb673ea2
KLuka <klukasocial@gmail.com>
2016-04-10 15:58:50 +0200

    disallow Cocktail Recipes with less than two ingredients

--- app/models/cocktail_recipe.rb
+++ app/models/cocktail_recipe.rb
@@ -1,3 +1,11 @@
+# from http://stackoverflow.com/a/36134208/1796645
+class CollectionLengthValidator < ActiveModel::Validations::LengthValidator
+  def validate_each(record, attribute, value)
+    value = value.respond_to?(:reject) ? value.reject(&:marked_for_destruction?) : value
+    super(record, attribute, value)
+  end
+end
+
 # This Model represents a Cocktail Recipe
 # name
 # cocktail_recipe_parts
@@ -5,6 +13,7 @@ class CocktailRecipe < ActiveRecord::Base
   has_many :cocktail_recipe_parts, inverse_of: :cocktail_recipe
   accepts_nested_attributes_for :cocktail_recipe_parts, reject_if: :all_blank, allow_destroy: true
   validates :name, presence: true, uniqueness: true
+  validates :cocktail_recipe_parts, collection_length: { minimum: 2 }
 
   def cost
     sum = 0
