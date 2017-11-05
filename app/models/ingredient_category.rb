428270556f6f1245d028f88c4877d86d913e0d11
Luka Lüdicke <kluedicke@gmail.com>
2017-11-05 21:38:58 +0100

    validate name presence in IngredientCategory

--- app/models/ingredient_category.rb
+++ app/models/ingredient_category.rb
@@ -1,4 +1,6 @@
 # This model represents the category of ingredients. Every ingredient needs to have one
 class IngredientCategory < ActiveRecord::Base
   has_many :ingredients
+
+  validates :name, presence: true, uniqueness: true
 end
