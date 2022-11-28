adc8ff7c17b58149d8a22def0a17372321516781
Luka Lüdicke <klukasocial+github@gmail.com>
2022-11-28 16:17:54 +0100

    Update rubocop gems

--- db/migrate/20161231165008_set_default_value_to_strict.rb
+++ db/migrate/20161231165008_set_default_value_to_strict.rb
@@ -2,6 +2,6 @@
 
 class SetDefaultValueToStrict < ActiveRecord::Migration[5.0]
   def change
-    change_column :cocktail_recipe_parts, :strict, :boolean, default: true
+    change_column :cocktail_recipe_parts, :strict, :boolean, default: true # rubocop:disable Rails/ReversibleMigration
   end
 end
